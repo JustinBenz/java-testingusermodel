029c4056e347a8c72b7d1c38c03c4c99e1079512
Justin Benz <jbenz156@gmail.com>
2021-01-27 20:04:00 -0500

    finished

---
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceTest.java
@@ -0,0 +1,56 @@
+package com.lambdaschool.usermodel.services;
+
+import com.lambdaschool.usermodel.UserModelApplicationTests;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = UserModelApplicationTests.class)
+public class UserServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findByNameContaining() {
+    }
+
+    @Test
+    public void findUserById() {
+    }
+
+    @Test
+    public void findByName() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void deleteAll() {
+    }
+}
